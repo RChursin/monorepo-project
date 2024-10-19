f3993df98fcfc43660251bf22f81c15c4e1ebabe
Cyber Junkie <cyberjunkie@Cybers-MacBook-Pro.local>
2024-10-19 16:58:15 -0700

    cql

--- apps/app-portfolio/src/app/components/main-content/main-content.tsx
+++ apps/app-portfolio/src/app/components/main-content/main-content.tsx
@@ -2,9 +2,5 @@ import React from 'react';
 import styles from './main-content.module.css';
 
 export default function MainContent({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles['main-content']}>
-      {children}
-    </div>
-  );
+  return <div className={styles['main-content']}>{children}</div>;
 }
